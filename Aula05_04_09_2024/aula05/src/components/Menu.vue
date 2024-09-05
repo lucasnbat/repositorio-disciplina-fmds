fccbc940a8f631ab36cfe84faba5d83762e23e31
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-09-04 20:34:25 -0400

    adiciona pasta da `aula05`

---
+++ Aula05_04_09_2024/aula05/src/components/Menu.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-list>
+    <v-list-subheader>
+      {{ titulo }}
+    </v-list-subheader>
+    <v-list-item prepend-icon="mdi-home" title="Home" />
+  </v-list>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  titulo: String,
+})
+</script>
---
+++ Aula05_04_09_2024/aula05/src/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <Menu titulo="Menu principal" />
+    </v-navigation-drawer>
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const drawer = ref(true)
+</script>
