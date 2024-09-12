d3b9cd6d2fb7667141ef34b3d9c9a8c0ce707a75
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-09-11 21:15:34 -0400

    adiciona novos arquivos > form funcionando com watch()

--- Aula06_11_09_2024/aula6/src/pages/form.vue
+++ Aula06_11_09_2024/aula6/src/pages/form.vue
@@ -6,11 +6,11 @@
       <v-spacer />
       <v-card-text>
         <v-form>
-          <v-text-field v-model="name" prepend-inner-icon="mdi-account" variant="underlined" label="Name" />
-          <v-text-field v-model="lastname" prepend-inner-icon="mdi-account-outline" variant="underlined"
+          <v-text-field v-model="user.name" prepend-inner-icon="mdi-account" variant="underlined" label="Name" />
+          <v-text-field v-model="user.lastName" prepend-inner-icon="mdi-account-outline" variant="underlined"
             label="Last name" />
-          <v-text-field v-model="email" prepend-inner-icon="mdi-email" variant="underlined" label="E-mail" />
-          <v-text-field v-model="password" prepend-inner-icon="mdi-lock" variant="underlined" label="Password" />
+          <v-text-field v-model="user.email" prepend-inner-icon="mdi-email" variant="underlined" label="E-mail" />
+          <v-text-field v-model="user.password" prepend-inner-icon="mdi-lock" variant="underlined" label="Password" />
 
           <v-list density="compact">
             <!-- bind (:nomeDoAtributo): serve para enviar variaveis do js para o template -->
@@ -38,22 +38,28 @@
 <script setup>
 import { ref, watch } from 'vue'
 
-const name = ref('')
-const lastname = ref('')
-const email = ref('')
-const password = ref('')
+const user = ref({
+  name: '',
+  lastName: '',
+  email: '',
+  password: '',
+})
 const ready = ref(false)
 
 const passwordRequirements = ref([
   { name: 'Password need to have a minimal of eight characters', checked: false },
   { name: 'Password need to have at last one number', checked: false },
-  { name: 'Password need to have at last one character from the alphabet', checked: false },
-  { name: 'Password need to have at last one special character', checked: false }
+  { name: 'Password need to have at last one lowercase letter', checked: false },
+  { name: 'Password need to have at last one special character', checked: false },
+  { name: 'Password need to have at last one uppercase letter', checked: false }
 ])
 
-watch(password, (val) => {
+watch(() => user.value.password, (val) => {
   passwordRequirements.value[0].checked = val.length >= 8 //substitui o checkd do primeiro objeto por true
   passwordRequirements.value[1].checked = !!val.match(/[0-9]/)
-  passwordRequirements.value[2].checked = !!val.match(/[a-zA-Z]/)
+  passwordRequirements.value[2].checked = !!val.match(/[a-z]/)
+  passwordRequirements.value[3].checked = !!val.match(/[^\w]/)
+  passwordRequirements.value[4].checked = !!val.match(/[A-Z]/)
+
 })
 </script>
