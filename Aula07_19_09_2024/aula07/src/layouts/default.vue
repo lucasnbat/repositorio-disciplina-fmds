1c0f3343ecf2eb77bf4b9a4d39f80606fd0b1d56
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-09-18 19:36:30 -0400

    feat: add aula7 archives

---
+++ Aula07_19_09_2024/aula07/src/layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar class="px-3" density="compact" flat>
+      <v-avatar class="hidden-md-and-up" color="grey-darken-1" size="32"></v-avatar>
+
+      <v-spacer></v-spacer>
+
+      <v-tabs color="grey-darken-2" centered>
+        <v-tab text="Home" />
+        <v-tab text="Login" />
+        <v-tab text="Dashboard" />
+      </v-tabs>
+
+      <v-spacer></v-spacer>
+
+      <v-avatar class="hidden-sm-and-down" color="grey-darken-1" size="32"></v-avatar>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="14">
+            <v-sheet min-height="70vh" rounded="lg">
+              <container>
+                <router-view />
+              </container>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+//
+</script>
---
+++ Aula07_19_09_2024/aula07/src/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-text>Tchain</v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup>
+//
+</script>
